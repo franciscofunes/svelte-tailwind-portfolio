45951f50d0978842245373fccd1ffac6666dbe9c
franciscofunes <ffunes90@gmail.com>
2022-06-11 13:22:42 -0300

    more styles

--- src/lib/components/project-card.svelte
+++ src/lib/components/project-card.svelte
@@ -5,8 +5,8 @@
 	export let description = '';
 </script>
 
-<div class="relative group card shadow-2xl col-span-2">
-	<img src={url} alt={name} class="object-cover h-full" />
+<!-- <div class="relative group card shadow-2xl col-span-2 m-2">
+	<img src={url} alt={name} class="object-cover h-full " />
 	<a href={`/projects/${slug}`}>
 		<div
 			class="absolute bottom-0 left-0 right-0 lg:opacity-0 group-hover:opacity-100 bg-primary p-4 duration-300 text-primary-content"
@@ -15,6 +15,24 @@
 			<p class="text-sm lg:text-xl">
 				{description.slice(0, 80)}...
 			</p>
+		</div>
+	</a>
+</div> -->
+
+<div class="card card-compact  shadow-xl ">
+	<figure><img src={url} alt={name} /></figure>
+	<a href={`/projects/${slug}`}>
+		<div class="card-body ">
+			<h2 class="font-bold lg:text-xl">{name}</h2>
+			<p class="text-sm lg:text-xl">
+				{description.slice(0, 80)}...
+			</p>
+			<div class="card-actions justify-end">
+				<button
+					class="btn btn-primary  hover:opacity-100 duration-300 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
+					src={`/projects/${slug}`}>Conocer más...</button
+				>
+			</div>
 		</div>
 	</a>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,9 +33,18 @@
 	export let projects;
 </script>
 
-<h1>Recent Projects by Me</h1>
-
-<div>
+<div
+	class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5"
+>
+	<article class="prose lg:prose-xl">
+		<h1>
+			Mis <span
+				class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-gray-500 relative inline-block"
+			>
+				<span class="relative text-white">proyectos</span>
+			</span> recientes
+		</h1>
+	</article>
 	{#each projects as { name, slug, description, image }}
 		<ProjectCard {name} {description} url={image[0].url} {slug} />
 	{/each}
