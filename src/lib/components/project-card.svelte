7ba54119ae2232ccd52f585a20239e84ab2c629e
franciscofunes <ffunes90@gmail.com>
2022-08-13 15:00:27 -0300

    eye icon in individual file

--- src/lib/components/project-card.svelte
+++ src/lib/components/project-card.svelte
@@ -1,4 +1,6 @@
 <script>
+	import EyeIcon from './icons/eye-icon.svelte';
+
 	export let url = '';
 	export let name = '';
 	export let slug = '';
@@ -32,27 +34,7 @@
 					class="btn btn-primary shadow hover:opacity-100 duration-300 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
 					src={`/projects/${slug}`}
 				>
-					<svg
-						x-show="!isTabsActive"
-						class="w-6 h-6 text-white animate-ping"
-						fill="none"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-						/>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-						/>
-					</svg>
+					<EyeIcon />
 					<span class="m-2">Ver</span>
 				</button>
 			</div>
